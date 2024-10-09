2dd6c1fa2190fa98fd67bab4153b383a55058974
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-10-09 16:15:16 +0200

    Add function AST

---
+++ ast.c
@@ -0,0 +1,81 @@
+#include "interpreter.h"
+
+
+linkAST* create_link_number(double value) {
+
+    linkAST* link = (linkAST*)malloc(sizeof(linkAST));
+    numberLink* number = (numberLink*)malloc(sizeof(numberLink));
+
+    number->value = value;
+
+    link->type = wayNumber;
+    link->number = number;
+    link->operator = NULL;
+
+    return link;
+}
+
+
+linkAST* create_link_operator(operatorType op, linkAST* left, linkAST* right) {
+
+    linkAST* link = (linkAST*)malloc(sizeof(linkAST));
+    operatorLink* operator = (operatorLink*)malloc(sizeof(operatorLink));
+
+
+    operator->op = op;
+    operator->left = left;
+    operator->right = right;
+
+
+    link->type = wayOperator;
+    link->operator = operator;
+    link->number = NULL;
+
+    return link;
+}
+
+
+double checkAst(linkAST* link) {
+    if (link->type == wayNumber) {
+
+        return link->number->value;
+
+    } else if (link->type == wayOperator) {
+
+        double left = checkAst(link->operator->left);
+        double right = checkAst(link->operator->right);
+
+
+        switch (link->operator->op) {
+            case addOP:
+                return left + right;
+            case subOP:
+                return left - right;
+            case mulOP:
+                return left * right;
+            case divOP:
+                return left / right;
+            default:
+                fprintf(stderr, "unknown opérator\n");
+                exit(EXIT_FAILURE);
+        }
+    }
+    return 0;  
+}
+
+
+
+void freeAst(linkAST* link) {
+    if (link->type == wayOperator) {
+        
+        freeAst(link->operator->left);
+        freeAst(link->operator->right);
+        
+        free(link->operator);
+    } else if (link->type == wayNumber) {
+        
+        free(link->number);
+    }
+    
+    free(link);
+}
--- interpreter.h
+++ interpreter.h
@@ -4,9 +4,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 // file.c
 long get_Size_File(FILE *str);
 char *copy_file(char *commands, FILE *file_commands);
 char *check_and_copy_file(char *commands, char **av);
 
+
+
+//ast.c
+
+typedef enum {
+    wayNumber,
+    wayOperator
+} Typelink;
+
+typedef enum {
+    addOP,
+    subOP,
+    mulOP,
+    divOP
+} operatorType;
+
+
+
+typedef struct {
+    double value;
+} numberLink;
+
+
+typedef struct {
+    operatorType op;
+    struct linkAST* right;
+    struct linkAST* left;
+} operatorLink;
+
+
+typedef struct linkAST {
+    Typelink type;
+    numberLink* number;
+    operatorLink* operator;
+} linkAST;
+
+
+
+linkAST* create_link_number(double value);
+linkAST* create_link_operator(operatorType op, linkAST* left, linkAST* right);
+double checkAst(linkAST* link);
+void freeAst(linkAST* link);
+
+
 #endif
+
+
