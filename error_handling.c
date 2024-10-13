f367469d4a9b265fdb52b62fcd7cd9ff4330e6ec
Makav77 <160429140+Makav77@users.noreply.github.com>
2024-10-13 16:01:26 +0200

    Update error_handling.c

--- error_handling.c
+++ error_handling.c
@@ -1,7 +1,7 @@
 #include "header/include.h"
 
 int error_character(const char *commands) {
-    const char *allowed_char = "><(){}=+*-/";
+    const char *allowed_char = "><(){}=+*-/ ";
 
     for (int x = 0; commands[x] != '\0'; x++) {
         if (!isalnum(commands[x]) && !strchr(allowed_char, commands[x]))
